aec8ea0e46baea42da6d0336e49737a03f2c861f
RobertWeaver <robusa8818@gmail.com>
2024-08-28 01:46:47 -0600

    update AspireKeycloak.Web

--- AspireKeycloak.Web/Program.cs
+++ AspireKeycloak.Web/Program.cs
@@ -1,18 +1,51 @@
+using System.IdentityModel.Tokens.Jwt;
+using AspireKeycloak.Web;
 using AspireKeycloak.Web.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add service defaults & Aspire components.
 builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddOutputCache();
+
+builder.Services.AddHttpContextAccessor()
+                .AddTransient<AuthorizationHandler>();
+
+builder.Services.AddHttpClient<WeatherApiClient>(client =>
+    {
+        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+        client.BaseAddress = new("https+http://apiservice");
+    })
+    .AddHttpMessageHandler<AuthorizationHandler>();
+
+var oidcScheme = OpenIdConnectDefaults.AuthenticationScheme;
+
+builder.Services.AddAuthentication(oidcScheme)
+                .AddKeycloakOpenIdConnect("keycloak", realm: "WeatherShop", oidcScheme, options =>
+                {
+                    options.ClientId = "WeatherWeb";
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.Scope.Add("weather:all");
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters.NameClaimType = JwtRegisteredClaimNames.Name;
+                    options.SaveTokens = true;
+                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+builder.Services.AddCascadingAuthenticationState();
+
 var app = builder.Build();
 
-app.MapDefaultEndpoints();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -25,7 +58,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.UseOutputCache();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapDefaultEndpoints();
+app.MapLoginAndLogout();
 
 app.Run();
