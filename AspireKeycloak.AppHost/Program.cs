9519543b09098e9c32d0115f2767f1be441971f9
RobertWeaver <robusa8818@gmail.com>
2024-08-28 01:27:30 -0600

    add realm

--- AspireKeycloak.AppHost/Program.cs
+++ AspireKeycloak.AppHost/Program.cs
@@ -1,5 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.AspireKeycloak_ApiService>("aspirekeycloak-apiservice");
+var keycloak = builder.AddKeycloak("keycloak", 8080)
+                      .WithDataVolume()
+                      .WithRealmImport("../realms");
+
+var apiService = builder.AddProject<Projects.AspireKeycloak_ApiService>("apiservice")
+                        .WithReference(keycloak);
+
+// builder.AddProject<Projects.Keycloak_Web>("webfrontend")
+//     .WithExternalHttpEndpoints()
+//     .WithReference(keycloak)
+//     .WithReference(apiService);
 
 builder.Build().Run();
+
