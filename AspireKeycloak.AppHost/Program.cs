86567c6599350e7b3c1c67d370e5b990f5d467dd
RobertWeaver <robusa8818@gmail.com>
2024-08-30 02:29:47 -0600

    update to Aspire 8.2

--- AspireKeycloak.AppHost/Program.cs
+++ AspireKeycloak.AppHost/Program.cs
@@ -2,22 +2,16 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var keycloak = builder.AddKeycloak("keycloak", 8080)
                       .WithDataVolume()
+                      .WithImageTag("latest")
                       .WithRealmImport("../realms");
 
 var apiService = builder.AddProject<Projects.AspireKeycloak_ApiService>("apiservice")
                         .WithReference(keycloak);
 
-// builder.AddProject<Projects.Keycloak_Web>("webfrontend")
-//     .WithExternalHttpEndpoints()
-//     .WithReference(keycloak)
-//     .WithReference(apiService);
-
-builder.AddProject<Projects.AspireKeycloak_Web>("aspirekeycloak-web");
-
-// builder.AddProject<Projects.Keycloak_Web>("webfrontend")
-//     .WithExternalHttpEndpoints()
-//     .WithReference(keycloak)
-//     .WithReference(apiService);
+builder.AddProject<Projects.AspireKeycloak_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(keycloak)
+    .WithReference(apiService);
 
 builder.Build().Run();
 
